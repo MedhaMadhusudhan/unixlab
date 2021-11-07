b12991c3ae7c0c9e4c63606e0d1e41082173e52d
MedhaMadhusudhan <56746382+MedhaMadhusudhan@users.noreply.github.com>
2021-11-07 19:33:01 +0530

    Create factorial.sh

---
+++ factorial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+prod=1
+echo "enter number whose factorial is to be found"
+read num
+
+for (( count=$num; count>0; count-- )); do
+	prod=`expr $prod \* $count`
+done
+
+echo "factorial is $prod"
