23e207814995b1ad43d89d45ca74761a6f48ac44
MedhaMadhusudhan <56746382+MedhaMadhusudhan@users.noreply.github.com>
2021-11-07 19:20:01 +0530

    Create posnegzero.sh

---
+++ posnegzero.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter a number"
+read num
+
+if [ $num -gt 0 ]; then
+	echo "number is positive"
+elif [ $num -eq 0 ]; then 
+	echo "number is zero"
+else
+	echo "number is negative"
+fi
